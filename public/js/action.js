b08872377949f8c78033c16196b623746be7d5f4
victoricoma-soulcode <victor.icoma@soulcode.com>
2023-11-14 20:44:24 -0300

    Aula1411 Fundamentos de DOM HTML

--- public/js/action.js
+++ public/js/action.js
@@ -1,14 +1,22 @@
-function somaNumero(n1, n2, op){
-    let resultado=0;
-    if(op==="c"){
+function calculaNumeros(){
+    let n1 = parseInt(document.getElementById("usuario").value)
+    let n2 = parseInt(document.getElementById("senha").value)
+    let op = document.getElementById("operation").value
+    let resultado
+    if(op === "+"){
         resultado=(n1+n2)
-        console.table(resultado)
-        return
-    }
-
-    if(op==="a"){
-        resultado=(n1 - n2)
-        alert(resultado)
-        return
+        alert("Soma: "+n1+" "+op+" "+n2+" = "+resultado)
+    }else if(op === "-"){
+        resultado=(n1-n2)
+        alert("Subtração: "+n1+" "+op+" "+n2+" = "+resultado)   
+    }else if(op === "*"){
+        resultado=(n1*n2)
+        alert("Multiplicação: "+n1+" "+op+" "+n2+" = "+resultado)   
+    }else if(op === "/"){
+        resultado=(n1/n2)
+        alert("Divisão: "+n1+" "+op+" "+n2+" = "+resultado)   
+    }else if(op === "sqrt"){
+        resultado = Math.sqrt(n1+n2)
+        alert("Raiz: "+resultado)
     }
 }
