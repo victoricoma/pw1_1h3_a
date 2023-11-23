84d198271b2d358bd3c439f8d5ed76067bad672d
victoricoma-soulcode <victor.icoma@soulcode.com>
2023-11-22 22:38:50 -0300

    Aula2211 Arrays e Interações

--- public/js/action.js
+++ public/js/action.js
@@ -1,10 +1,16 @@
 import userLogin from './moduloDados.js'
 
 const login = document.getElementById("login")
+const cadastro = document.getElementById("cadastro")
 
 login.addEventListener("click", (e) => {
     let user = document.getElementById("usuario").value
     let senha = document.getElementById("senha").value
-    console.table(user, senha)
-    console.table(userLogin)
 })
+cadastro.addEventListener("click", (e) => {
+    var usuario =userLogin.find((userLogin) => userLogin.email === "victorsena3010@gmail.com")
+    console.log(usuario)
+})
+function encontraUsuario(value, index, array){
+    console.table(value.email)
+}
